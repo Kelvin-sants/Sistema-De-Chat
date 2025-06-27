22b747c4225b8c26f8706891bf483e01515ef65d
Kelvin Vieira <121833437+Kelvin-sants@users.noreply.github.com>
2025-06-27 17:13:09 -0300

    Adicionando ServidorChat.java
    ServidorChat.java é executado na maquina que será o servidor, é responsável por aceitar novos clientes e criar uma thread para cada cliente, permitindo que cada cliente execute em paralelo; servidor fica aberto esperando novas conexões o tempo todo.

---
+++ ServidorChat.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class ServidorChat {
+
+    private static final int PORTA = 12345;             //porta do servidor
+
+    public static void main(String[] args){
+
+        GerenciaUsuarios GerenciaUsuarios = new GerenciaUsuarios();              //Instancia Gerencia usuarios
+        GerenciaSalas GerenciaSalas = new GerenciaSalas();                      //Instacia Gerencia Salas
+        Socket socketCliente;
+        Scanner entrada = null;
+        
+        try(ServerSocket servidor = new ServerSocket(PORTA)){                        //tenta abrir o servidor na porta especifica
+            
+            System.out.println("Servidor Iniciado com sucesso na porta:" + PORTA);
+            
+            while(true){
+                
+                socketCliente = servidor.accept();                                                          //espera o usuario conectar no servidor
+                System.out.println("Novo cliente conectado, ID: " + socketCliente.getInetAddress());            //informa no servidor que um novo cliente se conectou
+                ClientHandler handler = new ClientHandler(socketCliente, GerenciaUsuarios, GerenciaSalas);      //cria uma nova ClientHandler para aquele cliente
+                Thread  threadHandler = new Thread(handler);                        //colocando handler em uma thread
+                threadHandler.start();                                            // Inicia a thread (vai executar o run() do handler)
+            }
+
+        }catch(IOException e){
+
+            System.err.println("Erro no servidor: " + e.getMessage());
+
+        }finally{
+
+            if(entrada != null){
+                entrada.close();
+            }
+            
+        }
+
+    }
+}
