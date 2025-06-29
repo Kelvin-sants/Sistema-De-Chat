bc7dd1adb5d7fed255e60faf8d76d874d1c7e1d8
brendda-thamyress <brenddathamyres06@gmail.com>
2025-06-29 11:53:15 -0300

    docs: adicionando comentários à classe usuário

--- Usuario.java
+++ Usuario.java
@@ -5,6 +5,7 @@ import java.io.PrintStream;
 
 public class Usuario{
 
+    //atributos:
     private String nome;
     private boolean ehAdm;
     private Sala salaAtual;
@@ -15,21 +16,25 @@ public class Usuario{
         salaAtual = null;
     }
 
+    //métodos:
+
+    //setters:
     public void setNome(String nome){
         this.nome = nome;
-    }
+    } //define o nome do usuário
 
     public void setStatusAdm(boolean setStatusAdm){
         this.ehAdm = setStatusAdm;
-    }
+    } // define o status do usuário 
 
     public void setSala(Sala sala){
         this.salaAtual = sala;
-    }
+    } // define a sala em que o usuario vai estar
 
+    //getters: 
     public String getNome(){
         return this.nome;
-    }
+    } 
 
     public boolean getStatusAdm(){
         return this.ehAdm;
